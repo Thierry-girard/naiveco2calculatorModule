523fd888bb5ea296df3acb10e977b847cd20aa43
Thierry Girard <girard.thierry80@gmail.com>
2018-05-02 10:08:52 +0200

    Refactoring

--- index.js
+++ index.js
@@ -48,36 +48,50 @@ function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
  * 
  * @returns {gpsPoint}
  */
-module.exports.gpsPoint = function(lat, long){
+function gpsPoint(lat, long){
     return {latitude : lat, longitude : long}
 }
 
 
-///////// Car CO2 emission Computing /////////
+///////// CO2 emission Computing /////////
 /**
- * Compute the average CO2 emission for a car for a given distance
+ * Compute the average CO2 emission for a given distance and a given transportation
  * 
  * @param {number} dist - distance in kilometers
+ * @param {String} locomotionType - Key of the emission level dictionary 
  * 
  * @returns {number} Co2 emission in gram
  */
-module.exports.emissionVoitureForDistance = function(dist) {
+function emissionForDistance(dist, locomotionType) {
     if(!dist){
         console.log("Undefined distance") 
         return -1;
     }
-    return dist * 1.2 * emissionLevel[voiture];
+    if(locomotionType == voiture){      
+        return dist * 1.2 * emissionLevel[locomotionType];
+    }
+    else if(locomotionType == metro  || locomotionType == rer   || locomotionType == tramway  || locomotionType == transilien   || locomotionType == bus){       
+        return dist * emissionLevel[locomotionType];
+    }
+    else if(locomotionType == marche   || locomotionType == velo){       
+        return 0;
+    }
+    else{
+        console.log("Undefined ")
+        return -2;
+    }
 };
 
 /**
- * Compute the average CO2 emission for a car between two gps point
+ * Compute the average CO2 emission for a given transportation between two gps point
  * 
  * @param {gpsPoint} start - starting point gps cordonate 
  * @param {gpsPoint} end - ending point gps coordonate
+ * @param {String} locomotionType - Key of the emission level dictionary 
  * 
  * @returns {number} Co2 emission in gram
  */
-module.exports.emissionVoitureFromGPSPoints = function(start, end) {
+function emissionFromGPSPoints(start, end, locomotionType) {
     if(!(start && start.latitude && start.longitude)){
         console.log("Undefined start point") 
         return -1;
@@ -85,215 +99,22 @@ module.exports.emissionVoitureFromGPSPoints = function(start, end) {
     if(!(end && end.latitude && end.longitude)){
         console.log("Undefined end point") 
         return -1;
-    }       
-    return module.exports.emissionVoitureForDistance(distanceInKmBetweenEarthCoordinates(start.latitude, start.longitude, end.latitude, end.longitude));
-};
-
-
-///////// Metro CO2 emission Computing /////////
-/**
- * Compute the average CO2 emission for a metro ride for a given distance
- * 
- * @param {number} dist - distance in kilometers
- * 
- * @returns {number} Co2 emission in gram
- */
-module.exports.emissionMetroForDistance = function(dist) {
-    if(!dist){
-        console.log("Undefined distance") 
-        return -1;
     }
-    return dist * emissionLevel[metro];
-};
-
-/**
- * Compute the average CO2 emission for a metro ride between two gps point
- * 
- * @param {gpsPoint} start - starting point gps cordonate 
- * @param {gpsPoint} end - ending point gps coordonate
- * 
- * @returns {number} Co2 emission in gram
- */
-module.exports.emissionMetroFromGPSPoints = function(start, end) {
-    if(!(start && start.latitude && start.longitude)){
-        console.log("Undefined start point") 
-        return -1;
-    }
-    if(!(end && end.latitude && end.longitude)){
-        console.log("Undefined end point") 
-        return -1;
-    }       
-    return module.exports.emissionMetroForDistance(distanceInKmBetweenEarthCoordinates(start.latitude, start.longitude, end.latitude, end.longitude));
-};
-
-///////// RER CO2 emission Computing /////////
-/**
- * Compute the average CO2 emission for a rer ride for a given distance
- * 
- * @param {number} dist - distance in kilometers
- * 
- * @returns {number} Co2 emission in gram
- */
-module.exports.emissionRERForDistance = function(dist) {
-    if(!dist){
-        console.log("Undefined distance") 
-        return -1;
-    }
-    return dist * emissionLevel[rer];
-};
-
-/**
- * Compute the average CO2 emission for a rer ride between two gps point
- * 
- * @param {gpsPoint} start - starting point gps cordonate 
- * @param {gpsPoint} end - ending point gps coordonate
- * 
- * @returns {number} Co2 emission in gram
- */
-module.exports.emissionRERFromGPSPoints = function(start, end) {
-    if(!(start && start.latitude && start.longitude)){
-        console.log("Undefined start point") 
-        return -1;
-    }
-    if(!(end && end.latitude && end.longitude)){
-        console.log("Undefined end point") 
-        return -1;
-    }       
-    return module.exports.emissionRERForDistance(distanceInKmBetweenEarthCoordinates(start.latitude, start.longitude, end.latitude, end.longitude));
+    return module.exports.emissionForDistance(distanceInKmBetweenEarthCoordinates(start.latitude, start.longitude, end.latitude, end.longitude), locomotionType);
 };
 
 
-///////// Tramway CO2 emission Computing /////////
-/**
- * Compute the average CO2 emission for a tramway ride for a given distance
- * 
- * @param {number} dist - distance in kilometers
- * 
- * @returns {number} Co2 emission in gram
- */
-module.exports.emissionTramwayForDistance = function(dist) {
-    if(!dist){
-        console.log("Undefined distance") 
-        return -1;
-    }
-    return dist * emissionLevel[tramway];
-};
-
-/**
- * Compute the average CO2 emission for a tramway ride between two gps point
- * 
- * @param {gpsPoint} start - starting point gps cordonate 
- * @param {gpsPoint} end - ending point gps coordonate
- * 
- * @returns {number} Co2 emission in gram
- */
-module.exports.emissionTramwayFromGPSPoints = function(start, end) {
-    if(!(start && start.latitude && start.longitude)){
-        console.log("Undefined start point") 
-        return -1;
-    }
-    if(!(end && end.latitude && end.longitude)){
-        console.log("Undefined end point") 
-        return -1;
-    }       
-    return module.exports.emissionTramwayForDistance(distanceInKmBetweenEarthCoordinates(start.latitude, start.longitude, end.latitude, end.longitude));
-};
-
-///////// Bus CO2 emission Computing /////////
-/**
- * Compute the average CO2 emission for a bus ride for a given distance
- * 
- * @param {number} dist - distance in kilometers
- * 
- * @returns {number} Co2 emission in gram
- */
-function emissionBusForDistance(dist) {
-    if(!dist){
-        console.log("Undefined distance") 
-        return -1;
-    }
-    return dist * emissionLevel[bus];
-};
-module.exports.emissionBusForDistance = emissionBusForDistance;
-
-/**
- * Compute the average CO2 emission for a bus ride between two gps point
- * 
- * @param {gpsPoint} start - starting point gps cordonate 
- * @param {gpsPoint} end - ending point gps coordonate
- * 
- * @returns {number} Co2 emission in gram
- */
-function emissionBusFromGPSPoints(start, end) {
-    if(!(start && start.latitude && start.longitude)){
-        console.log("Undefined start point") 
-        return -1;
-    }
-    if(!(end && end.latitude && end.longitude)){
-        console.log("Undefined end point") 
-        return -1;
-    }       
-    return emissionBusForDistance(distanceInKmBetweenEarthCoordinates(start.latitude, start.longitude, end.latitude, end.longitude));
-};
-module.exports.emissionBusFromGPSPoints = emissionBusFromGPSPoints;
-
-///////// Transilien CO2 emission Computing /////////
-/**
- * Compute the average CO2 emission for a transilien ride for a given distance
- * 
- * @param {number} dist - distance in kilometers
- * 
- * @returns {number} Co2 emission in gram
- */
-function emissionTransilienForDistance(dist) {
-    if(!dist){
-        console.log("Undefined distance") 
-        return -1;
-    }
-    return dist * emissionLevel[transilien];
-};
-module.exports.emissionTransilienForDistance = emissionTransilienForDistance;
-
-/**
- * Compute the average CO2 emission for a transilien ride between two gps point
- * 
- * @param {gpsPoint} start - starting point gps cordonate 
- * @param {gpsPoint} end - ending point gps coordonate
- * 
- * @returns {number} Co2 emission in gram
- */
-function emissionTransilienFromGPSPoints(start, end) {
-    if(!(start && start.latitude && start.longitude)){
-        console.log("Undefined start point") 
-        return -1;
-    }
-    if(!(end && end.latitude && end.longitude)){
-        console.log("Undefined end point") 
-        return -1;
-    }       
-    return emissionTransilienForDistance(distanceInKmBetweenEarthCoordinates(start.latitude, start.longitude, end.latitude, end.longitude));
-};
-module.exports.emissionTransilienForDistance = emissionTransilienForDistance;
 
 
-///////// Vélo CO2 emission Computing /////////
-/**
- * Compute the average CO2 emission for a bike ride
- * 
- * @returns {number} Co2 emission in gram
- */
-function emissionVelo() {
-    return emissionLevel[velo];
-};
-module.exports.emissionVelo = emissionVelo;
+////////////// EXPORTS //////////////
+module.exports.marche = marche;
+module.exports.velo = velo;
+module.exports.rer = rer;
+module.exports.tramway = tramway;
+module.exports.bus = bus;
+module.exports.voiture = voiture;
+module.exports.transilien = transilien;
 
-///////// Marche CO2 emission Computing /////////
-/**
- * Compute the average CO2 emission for a walk
- * 
- * @returns {number} Co2 emission in gram
- */
-function emissionMarche() {
-    return emissionLevel[marche];
-};
-module.exports.emissionMarche = emissionMarche;
+module.exports.gpsPoint = gpsPoint;
+module.exports.emissionForDistance = emissionForDistance;
+module.exports.emissionFromGPSPoints = emissionFromGPSPoints;
